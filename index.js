a57682e2f9c723984316f64a0c72a52af8538825
Keely001 <thindiuke@gmail.com>
2023-04-18 15:31:13 +0300

    continuation

--- index.js
+++ index.js
@@ -9,15 +9,31 @@ function add_class(b){
     b.classList.toggle("active");
 }
 var swiper = new Swiper(".body1Swiper", {
+    loop:true,
     effect: "cube",
     grabCursor: true,
-    cubeEffect: {
-      slideShadows: true,
-      shadowOffset: 20,
-      shadowScale: 0.94,
-    },
+    // cubeEffect: {
+    //   slideShadows: false,
+    //   shadowOffset: 20,
+    //   shadowScale: 0.94,
+    // },
     pagination: {
       el: ".swiper-pagination",
+    },
+  });
+
+
+  var swiper = new Swiper(".swiper-features", {
+    slidesPerView: 3,
+    spaceBetween: 20,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
     },
   });
 
